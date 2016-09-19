a238749ada1e38f41bec31385c57fb597887a95c
maxvanweenen <maxvanweenen@gmail.com>
2016-09-19 17:29:46 +0200

    test slecht
    maybe

--- wp-content/plugins/contact-form-7/includes/js/scripts1933.js
+++ wp-content/plugins/contact-form-7/includes/js/scripts1933.js
@@ -434,5 +434,30 @@
 		_wpcf7.supportHtml5 = $.wpcf7SupportHtml5();
 		$('div.wpcf7 > form').wpcf7InitForm();
 	});
+	
+	BRUSHED.contactForm = function(){
+ $("#contact-submit").on('click',function() {
+  $contact_form = $('#contact-form');
+  
+  var fields = $contact_form.serialize();
+  
+  $.ajax({
+   type: "POST",
+   url: "_include/php/contact.php",
+   data: fields,
+   dataType: 'json',
+   success: function(response) {
+    
+    if(response.status){
+     $('#contact-form input').val('');
+     $('#contact-form textarea').val('');
+    }
+    
+    $('#response').empty().html(response.html);
+   }
+  });
+  return false;
+ });
+}
 
 })(jQuery);
